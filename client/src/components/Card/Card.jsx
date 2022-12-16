232b97619c1c4e36822be1f44d440d7119339b82
kespinak <kespinak@gmail.com>
2022-12-16 13:11:29 -0800

    connected frontend to strapi. THERE IS ONLY 1 PROBLEM...THE IMAGES FOR FEATURED & TRENDING PRODUCTS IS NOT RENDERING FO RSOME REASON (LOOK AT FREATUREDPRODUCTS.JSX & CARD.JSX

--- client/src/components/Card/Card.jsx
+++ client/src/components/Card/Card.jsx
@@ -7,14 +7,27 @@ const Card = ({item}) => {
     <Link to={`/product/${item.id}`} className='link'>
       <div className='card'>
         <div className="image">
-          {item.isNew && <span>New Season</span>}
-          <img src={item.img} alt="" className="mainImg" />
-          <img src={item.img2} alt="" className="secondImg" />
+          {item?.attributes.isNew && <span>New Season</span>}
+          <img 
+            src={process.env.REACT_APP_UPLOAD_URL + item.attributes?.img?.data?.attributes?.url} 
+            // src={toString(process.env.REACT_APP_UPLOAD_URL + item.attributes?.img?.data?.attributes?.url)}  // * NEED TO FIX THE FEATURED & TREDING PHOTOS NOT SHOWING UP
+            // src='http://localhost:1337/uploads/brownpants3_a761b2aa4b.png' // * NEED TO FIX THE FEATURED & TREDING PHOTOS NOT SHOWING UP
+            alt="" 
+            className="mainImg" 
+          />
+        
+          <img 
+            src={process.env.REACT_APP_UPLOAD_URL + item.attributes?.img2?.data?.attributes?.url}
+            // src={'http://localhost:1337/uploads/brownpants1_eed69bec4d.png'} // * NEED TO FIX THE FEATURED & TREDING PHOTOS NOT SHOWING UP
+            alt="" 
+            className="secondImg"
+          />
         </div>
-        <h2>{item.title}</h2>
+        <h2>{item?.attributes.title}</h2>
         <div className="prices">
-          <h3>${item.oldPrice}</h3>
-          <h3>${item.newPrice}</h3>
+          {/* sales tactic to make is seem like customer is getting a deal 20$ off */}
+          <h3>${item.oldPrice || item?.attributes.newPrice + 20}</h3>
+          <h3>${item?.attributes.newPrice}</h3>
         </div>
       </div>
     </Link>
--- client/src/components/FeaturedProducts/FeaturedProducts.jsx
+++ client/src/components/FeaturedProducts/FeaturedProducts.jsx
@@ -5,7 +5,7 @@ import axios from 'axios';
 
 const FeaturedProducts = ({type}) => {
 
-  const data = [
+  /* const data = [
     {
       id: 1,
       img: 'https://images.pexels.com/photos/1549200/pexels-photo-1549200.jpeg?auto=compress&cs=tinysrgb&w=1600',
@@ -38,30 +38,35 @@ const FeaturedProducts = ({type}) => {
       oldPrice: 19,
       newPrice: 15,
     },
-  ];
+  ]; */
 
-  const [products, setProducts] = useState([]);
+  const [data, setData] = useState([]);
 
   /* * NOTE THAT WE CAN USE REACT QUERY OR SOME OTHER LIBRARY...BUT SINCE WE ARE ONLY FETCHING DATA AND WE ARE NOT GOING TO USE 
   ANY POST METHODS...WE WILL KEEP IT SIMPLE AND JUST USE USEEFFECT HOOT TO FETCH DATA FROM THE BACKEND */
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const data = await axios.get(
-          process.env.REACT_APP_API_URL + '/products',
+        const response = await axios.get(
+          process.env.REACT_APP_API_URL + '/products?populate=*', // * NEED TO FIX THE FEATURED & TREDING PHOTOS NOT SHOWING UP
           {
             headers: {
               Authorization: 'bearer ' + process.env.REACT_APP_API_TOKEN,
             },
           }
         );
-        console.log(data);
+        setData(response.data.data);
+        // console.log(data, 'data1');
       } catch(error) {
         console.log(error);
       }
     };
     fetchData();
   }, []);
+
+  console.log(data, 'data2'); // * NEED TO FIX THE FEATURED & TREDING PHOTOS NOT SHOWING UP
+  console.log(JSON.stringify(process.env.REACT_APP_UPLOAD_URL)); // * NEED TO FIX THE FEATURED & TREDING PHOTOS NOT SHOWING UP
+  // console.log(process.env.REACT_APP_UPLOAD_URL.toString()); // * NEED TO FIX THE FEATURED & TREDING PHOTOS NOT SHOWING UP
 
   return (
     <div className='featuredProducts'>
@@ -74,6 +79,7 @@ const FeaturedProducts = ({type}) => {
         <p>description of featured &/or trending</p>
       </div>
 
+      {/* sending to card component */}
       <div className="bottom">
         {data.map((item) => (
           <Card item={item} key={item.id} /> 
